20157f4993d2f855289c1a637e1fe3b2acac7994
XeryusTC <armadillo@onenetbeyond.org>
2016-11-19 16:31:01 +0100

    Set up continuous integration and code coverage
    - CI is supplied by Travis CI
    - Code coverage is supplied by Codecov

--- accountant/accountant/settings/ci.py
+++ accountant/accountant/settings/ci.py
@@ -1,2 +1,17 @@
 # -*- coding: utf-8 -*-
 from .base import *
+
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "accountant_ci",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
+    }
+}
+
+SECRET_KEY = "TestMe"
