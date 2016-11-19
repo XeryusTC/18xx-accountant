4c72971ac7e9a71e02b1dfe44dbf2f5a13de255f
XeryusTC <armadillo@onenetbeyond.org>
2016-11-19 12:52:35 +0100

    Create FT to start new game

--- accountant/functional_tests/pages/game.py
+++ accountant/functional_tests/pages/game.py
@@ -1,5 +1,10 @@
 # -*- coding: utf-8 -*-
-from page_objects import PageObject, MultiPageElement
+from page_objects import PageObject, PageElement, MultiPageElement
 
 class Homepage(PageObject):
     stylesheets = MultiPageElement(tag_name='link')
+    start_button = PageElement(name='new_game')
+
+
+class Housekeeping(PageObject):
+    game_name = PageElement(name='game_name')
--- accountant/functional_tests/test_homepage.py
+++ accountant/functional_tests/test_homepage.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from .base import FunctionalTestCase
+from .pages import game
 
 class HomePageTest(FunctionalTestCase):
     def test_create_game(self):
@@ -8,4 +9,11 @@ class HomePageTest(FunctionalTestCase):
         # She sees that the title of the browser contains '18xx Accountant'
         self.assertEqual(self.browser.title, '18xx Accountant')
 
-        self.fail ('Finish test')
+        # There is a button that says "Start new game", she clicks it
+        page = game.Homepage(self.browser)
+        self.assertEqual(page.start_button.text, 'Start new game')
+        page.start_button.click()
+
+        # She lands on a new page, it lists a code for the game name
+        housekeeping = game.Housekeeping(self.browser)
+        self.assertEqual(len(housekeeping.game_name.text), 4)
