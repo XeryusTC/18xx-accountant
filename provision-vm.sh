dfce45f56fbca100c67fa48b69257c2363a4d43f
XeryusTC <armadillo@onenetbeyond.org>
2016-12-06 21:40:45 +0100

    Add NodeJS and NPM to the VM, only install PhantomJS when required

--- provision-vm.sh
+++ provision-vm.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+# Install NodeJS and NPM
+if ! command -v node 2>/dev/null; then
+	NODE_KEY="9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280"
+	gpg --keyserver pgp.mit.edu --recv-keys $NODE_KEY
+	gpg -a --export $NODE_KEY | apt-key add -
+	echo 'deb http://deb.nodesource.com/node_6.x jessie main' > /etc/apt/sources.list.d/nodesource.list
+fi
+
 apt-get update
 apt-get install -y python3 python3-pip python3-virtualenv virtualenv git \
-	gettext postgresql-9.4 postgresql-server-dev-9.4 libfontconfig
+	gettext postgresql-9.4 postgresql-server-dev-9.4 libfontconfig \
+	nodejs
 
 # Download and install PhantomJS
-wget --quiet -O /tmp/phantomjs.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
-tar -xjC /tmp/ -f /tmp/phantomjs.tar.bz2
-mv /tmp/phantomjs-2.1.1-linux-x86_64/ /usr/local/share/phantomjs-2.1.1/
-ln -s /usr/local/share/phantomjs-2.1.1/bin/phantomjs /usr/bin/phantomjs
+if ! command -v phantomjs 2>/dev/null; then
+	wget --quiet -O /tmp/phantomjs.tar.bz2 https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2
+	tar -xjC /tmp/ -f /tmp/phantomjs.tar.bz2
+	mv /tmp/phantomjs-2.1.1-linux-x86_64/ /usr/local/share/phantomjs-2.1.1/
+	ln -s /usr/local/share/phantomjs-2.1.1/bin/phantomjs /usr/bin/phantomjs
+fi
 
 # Set up PostgreSQL
 if [[ $(sudo su - postgres -c 'psql -lqt | cut -d \| -f 1 | grep -w accountant | wc -l') != "1" ]]; then
