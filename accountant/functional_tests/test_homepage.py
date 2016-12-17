06b6b2fa0d8f7d8e4ef35068c3aa32cd39ac04c3
XeryusTC <armadillo@onenetbeyond.org>
2016-12-17 19:46:16 +0100

    Add FT to test if angular is loaded

--- accountant/functional_tests/test_homepage.py
+++ accountant/functional_tests/test_homepage.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
+import unittest
 from .base import FunctionalTestCase
 from .pages import game
 
 class HomePageTest(FunctionalTestCase):
+    @unittest.skip
     def test_create_game(self):
         # Alice is a user who visits the website
         self.browser.get(self.live_server_url)
@@ -18,3 +20,10 @@ class HomePageTest(FunctionalTestCase):
         self.assertIn('/en/game/', self.browser.current_url)
         housekeeping = game.Housekeeping(self.browser)
         self.assertEqual(len(housekeeping.game_name.text), 4)
+
+    def test_loads_angular_application(self):
+        # Alice is a user who visits the website
+        self.browser.get(self.live_server_url)
+        # She sees that the Angular 2 app has loaded
+        app = self.browser.find_element_by_tag_name('app-root')
+        self.assertIn('app works!', app.text)
