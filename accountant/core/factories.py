c5046a3cfc480e5cfa44bb4260b69782573fe792
XeryusTC <armadillo@onenetbeyond.org>
2016-12-19 17:59:52 +0100

    Add factories for Player and Company models

--- accountant/core/factories.py
+++ accountant/core/factories.py
@@ -6,3 +6,17 @@ from . import models
 class GameFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = models.Game
+
+
+class PlayerFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Player
+
+    name = factory.Sequence(lambda n: 'Player %d' % n)
+
+
+class CompanyFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Company
+
+    name = factory.Sequence(lambda n: 'C%d' % n)
