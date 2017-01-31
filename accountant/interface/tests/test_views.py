56e535e53850c24ae6c34994fe644dead0429496
XeryusTC <armadillo@onenetbeyond.org>
2017-01-31 22:12:21 +0100

    Deduplicate code in interface tests

--- accountant/interface/tests/test_views.py
+++ accountant/interface/tests/test_views.py
@@ -9,42 +9,42 @@ from .. import forms
 from .. import views
 
 class MainPageTests(TestCase):
+    def setUp(self):
+        self.url = reverse('ui:main')
+        self.view = views.MainPageView.as_view()
+        self.factory = RequestFactory()
+
     def test_uses_correct_templates(self):
-        response = self.client.get(reverse('ui:main'))
+        response = self.client.get(self.url)
         self.assertTemplateUsed(response, 'base.html')
         self.assertTemplateUsed(response, 'interface/base.html')
         self.assertTemplateUsed(response, 'interface/index.html')
 
     def test_url_resolves_to_correct_view(self):
-        found = resolve(reverse('ui:main'))
-        self.assertEqual(found.func.__name__,
-            views.MainPageView.as_view().__name__)
+        found = resolve(self.url)
+        self.assertEqual(found.func.__name__, self.view.__name__)
 
     def test_main_page_uses_create_game_form(self):
-        factory = RequestFactory()
-        request = factory.get(reverse('ui:main'))
-        response = views.MainPageView.as_view()(request)
+        request = self.factory.get(self.url)
+        response = self.view(request)
         self.assertIsInstance(response.context_data['form'],
             forms.CreateGameForm)
 
     def test_main_page_creates_game_on_successful_POST_request(self):
         self.assertEqual(models.Game.objects.count(), 0)
-        factory = RequestFactory()
-        request = factory.post(reverse('ui:main'), data={})
-        views.MainPageView.as_view()(request)
+        request = self.factory.post(self.url, data={})
+        self.view(request)
         self.assertEqual(models.Game.objects.count(), 1)
 
     def test_main_page_redirects_to_game_after_creating_it(self):
-        factory = RequestFactory()
-        request = factory.post(reverse('ui:main'), data={})
-        response = views.MainPageView.as_view()(request)
+        request = self.factory.post(self.url, data={})
+        response = self.view(request)
         self.assertEqual(response.url,
             reverse('ui:game', kwargs={'uuid': models.Game.objects.last().pk}))
 
     def test_can_set_bank_cash_when_creating_game(self):
-        factory = RequestFactory()
-        request = factory.post(reverse('ui:main'), data={'bank_cash': 22})
-        response = views.MainPageView.as_view()(request)
+        request = self.factory.post(self.url, data={'bank_cash': 22})
+        response = self.view(request)
         game = models.Game.objects.last()
         self.assertEqual(game.cash, 22)
 
@@ -52,30 +52,28 @@ class MainPageTests(TestCase):
 class GameViewTests(TestCase):
     def setUp(self):
         self.game = factories.GameFactory()
+        self.url = reverse('ui:game', kwargs={'uuid': self.game.pk})
+        self.view = views.GameView.as_view()
+        self.factory = RequestFactory()
 
     def test_uses_correct_templates(self):
-        response = self.client.get(reverse('ui:game',
-            kwargs={'uuid': self.game.pk}))
+        response = self.client.get(self.url)
         self.assertTemplateUsed(response, 'base.html')
         self.assertTemplateUsed(response, 'interface/base.html')
         self.assertTemplateUsed(response, 'interface/game.html')
 
     def test_url_resolves_to_correct_view(self):
-        found = resolve(reverse('ui:game', kwargs={'uuid': self.game.pk}))
-        self.assertEqual(found.func.__name__,
-            views.GameView.as_view().__name__)
+        found = resolve(self.url)
+        self.assertEqual(found.func.__name__, self.view.__name__)
 
     def test_game_instance_is_added_to_context_data(self):
-        factory = RequestFactory()
-        request = factory.get(reverse('ui:game',
-            kwargs={'uuid': self.game.pk}))
-        response = views.GameView.as_view()(request, uuid=self.game.pk)
+        request = self.factory.get(self.url)
+        response = self.view(request, uuid=self.game.pk)
         self.assertEqual(response.context_data['game'], self.game)
 
     def test_returns_404_when_game_does_not_exist(self):
-        factory = RequestFactory()
-        request = factory.get(reverse('ui:game',
+        request = self.factory.get(reverse('ui:game',
             kwargs={'uuid': '00000000-0000-0000-0000-000000000000'}))
         with self.assertRaises(Http404):
-            response = views.GameView.as_view()(request,
+            response = self.view(request,
                 uuid='00000000-0000-0000-0000-000000000000')
