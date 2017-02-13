fdbca0a02ddf1961cab019655427f0210a847533
XeryusTC <armadillo@onenetbeyond.org>
2017-02-14 00:37:00 +0100

    Add all colors for companies

--- accountant/functional_tests/test_layout_and_styling.py
+++ accountant/functional_tests/test_layout_and_styling.py
@@ -10,3 +10,12 @@ class StylesheetTests(FunctionalTestCase):
 
         self.assertTrue(any('modestgrid.css' in s.get_attribute('href')
             for s in page.stylesheets))
+
+    def test_color_css_loaded(self):
+        # Create a game
+        self.browser.get(self.live_server_url)
+        page = game.Homepage(self.browser)
+        page.start_button.click()
+
+        self.assertTrue(any('color.css' in s.get_attribute('href')
+            for s in page.stylesheets))
