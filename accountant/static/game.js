254f7b6573871bf84b8ea181064a6789cd508abd
XeryusTC <armadillo@onenetbeyond.org>
2017-02-17 14:34:52 +0100

    Toggle detail divs when clicking a player

--- accountant/static/game.js
+++ accountant/static/game.js
@@ -2,4 +2,9 @@ $(document).ready(function() {
 	$("input[type='radio'][data-color-select='true']").change(function(e) {
 		$("#" + $(this).data('target')).val($(this).val());
 	});
+
+	$(".player").on('click', function(e) {
+		$('.detail').hide();
+		$(this).find(".detail").show();
+	});
 });
