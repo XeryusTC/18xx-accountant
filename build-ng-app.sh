67a9cf94fa5e2109fad5f23f804738c816d2c2d1
XeryusTC <armadillo@onenetbeyond.org>
2017-03-11 12:01:27 +0100

    Script to build and move Angular app to Django

---
+++ build-ng-app.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+cd ngApp
+ng build $@
+cd ..
+mv -v ngApp/dist/index.html accountant/templates/ng/index.html
+cp -v ngApp/dist/*.js accountant/static/
+cp -vR ngApp/dist/static/ accountant/static/
