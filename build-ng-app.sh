06f1370bf9cf676de03f5fb1be5e4d8dd8e01979
XeryusTC <armadillo@onenetbeyond.org>
2017-03-15 01:21:50 +0100

    Serve Angular files from their own directory

--- build-ng-app.sh
+++ build-ng-app.sh
@@ -2,5 +2,7 @@
 cd ngApp
 ng build $@
 cd ..
+mkdir -p accountant/templates/ng/
+mkdir -p accountant/static/ng/
 mv -v ngApp/dist/index.html accountant/templates/ng/index.html
-cp -v ngApp/dist/*.js accountant/static/
+cp -v ngApp/dist/*.js accountant/static/ng/
