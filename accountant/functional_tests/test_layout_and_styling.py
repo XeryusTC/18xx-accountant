667a449a9a6c175998ccf115c54e0a1da378536c
XeryusTC <armadillo@onenetbeyond.org>
2017-03-25 14:37:05 +0100

    We don't need these javascript and css bits anymore

--- accountant/functional_tests/test_layout_and_styling.py
+++ accountant/functional_tests/test_layout_and_styling.py
@@ -20,35 +20,3 @@ class StylesheetTests(FunctionalTestCase):
 
         self.assertTrue(any('css/main.css' in s.get_attribute('href')
             for s in page.stylesheets))
-
-
-class JavascriptTests(FunctionalTestCase):
-    @unittest.expectedFailure
-    def test_jquery_loaded(self):
-        self.story('Go to the company creation page')
-        self.browser.get(self.live_server_url)
-        homepage = game.Homepage(self.browser)
-        homepage.start_button.click()
-        game_page = game.GamePage(self.browser)
-        game_page.add_company_link.click()
-
-        self.assertTrue(any('jquery' in s.get_attribute('src')
-            for s in homepage.scripts))
-
-    @unittest.expectedFailure
-    def test_game_js_loaded(self):
-        self.story('Go to the company creation page')
-        self.browser.get(self.live_server_url)
-        homepage = game.Homepage(self.browser)
-        homepage.start_button.click()
-
-        self.story('game.js should be loaded on the main game page')
-        self.assertTrue(any('static/game.js' in s.get_attribute('src')
-            for s in homepage.scripts))
-
-        game_page = game.GamePage(self.browser)
-        game_page.add_company_link.click()
-
-        self.story('game.js should be loaded on the add company page')
-        self.assertTrue(any('static/game.js' in s.get_attribute('src')
-            for s in homepage.scripts))
