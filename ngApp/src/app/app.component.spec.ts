2adb1d272397b0854ac60e5874ab24de12f1abfd
XeryusTC <armadillo@onenetbeyond.org>
2017-03-27 14:33:23 +0200

    Test app component

--- ngApp/src/app/app.component.spec.ts
+++ ngApp/src/app/app.component.spec.ts
@@ -1,32 +1,35 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { By }                               from '@angular/platform-browser';
+
+import { RouterOutletStubComponent } from './testing/router-stubs';
 
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
-  }));
+	let fixture: ComponentFixture<AppComponent>;
+	let comp: AppComponent;
+	let el: HTMLElement;
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
+	beforeEach(async(() => {
+		TestBed.configureTestingModule({
+			declarations: [
+				AppComponent,
+				RouterOutletStubComponent
+			],
+		}).compileComponents();
+	}));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AppComponent);
+		comp = fixture.componentInstance;
+	});
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
-  }));
+	it('should create the app', async(() => {
+		expect(comp).toBeTruthy();
+	}));
+
+	it(`should have a router outlet`, async(() => {
+		el = fixture.debugElement.query(By.css('router-outlet')).nativeElement;
+		expect(el).toBeTruthy();
+	}));
 });
