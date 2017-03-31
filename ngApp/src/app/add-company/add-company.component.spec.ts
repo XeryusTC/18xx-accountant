c99d32787a0d7d581f2e5bc0f20fb2a4a6664d09
XeryusTC <armadillo@onenetbeyond.org>
2017-03-31 14:55:46 +0200

    Test the add company component logic

--- ngApp/src/app/add-company/add-company.component.spec.ts
+++ ngApp/src/app/add-company/add-company.component.spec.ts
@@ -1,25 +1,53 @@
+import { NO_ERRORS_SCHEMA }                 from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Title, By }                        from '@angular/platform-browser';
 
+import { ActivatedRoute, ActivatedRouteStub } from '../testing/router-stubs';
 import { AddCompanyComponent } from './add-company.component';
 
 describe('AddCompanyComponent', () => {
-  let component: AddCompanyComponent;
-  let fixture: ComponentFixture<AddCompanyComponent>;
+	let component: AddCompanyComponent;
+	let fixture: ComponentFixture<AddCompanyComponent>;
+	let activatedRoute: ActivatedRouteStub;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AddCompanyComponent ]
-    })
-    .compileComponents();
-  }));
+	beforeEach(async(() => {
+		activatedRoute = new ActivatedRouteStub();
+		TestBed.configureTestingModule({
+			declarations: [AddCompanyComponent],
+			schemas: [NO_ERRORS_SCHEMA],
+			providers: [
+				{provide: ActivatedRoute, useValue: activatedRoute}
+			]
+		})
+		.compileComponents();
+	}));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AddCompanyComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+	beforeEach(() => {
+		fixture = TestBed.createComponent(AddCompanyComponent);
+		component = fixture.componentInstance;
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		fixture.detectChanges();
+		expect(component).toBeTruthy();
+	});
+
+	it('should set the page title to Add company', () => {
+		let titleService = fixture.debugElement.injector.get(Title);
+		let spy = spyOn(titleService, 'setTitle');
+		fixture.detectChanges();
+		expect(spy.calls.any()).toBe(true);
+	});
+
+	it('should get the game uuid from the current rout', () => {
+		expect(component.game_id).toBe(undefined);
+		activatedRoute.testParams = {uuid: 'test uuid'};
+		fixture.detectChanges();
+		expect(component.game_id).toBe('test uuid');
+	});
+
+	it('should include the menu', () => {
+		let menu = fixture.debugElement.query(By.css('menu')).nativeElement;
+		expect(menu).toBeTruthy();
+	});
 });
--- ngApp/src/app/testing/router-stubs.ts
+++ ngApp/src/app/testing/router-stubs.ts
@@ -1,7 +1,46 @@
-import { Component } from '@angular/core';
+// Convenience export
+export { ActivatedRoute } from '@angular/router';
+
+import { Component, Directive, Injectable, Input } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
 	selector: 'router-outlet',
 	template: ''
 })
 export class RouterOutletStubComponent { }
+
+@Directive({
+	selector: '[routerLink]',
+	host: {
+		'(click)': 'onClick()'
+	}
+})
+export class RouterLinkStubDirective {
+	@Input('routerLink') linkParams: any;
+	navigatedTo: any = null;
+
+	onClick() {
+		this.navigatedTo = this.linkParams;
+	}
+}
+
+@Injectable()
+export class ActivatedRouteStub {
+	// ActivatedRoute.params is Observable
+	private subject = new BehaviorSubject(this.testParams);
+	params = this.subject.asObservable();
+
+	// Test parameters
+	private _testParams: {};
+	get testParams() { return this._testParams; }
+	set testParams(params: {}) {
+		this._testParams = params;
+		this.subject.next(params);
+	}
+
+	// ActivatedRoute.snapshot.params
+	get snapshot() {
+		return { params: this.testParams };
+	}
+}
