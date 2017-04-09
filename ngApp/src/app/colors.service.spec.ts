f45ce6a9f7d2fbc709bae86cbb73f5102e8efb1e
XeryusTC <armadillo@onenetbeyond.org>
2017-04-09 10:36:32 +0200

    Test error handling of color service [ci skip]

--- ngApp/src/app/colors.service.spec.ts
+++ ngApp/src/app/colors.service.spec.ts
@@ -1,4 +1,5 @@
-import { TestBed, inject, async } from '@angular/core/testing';
+import { TestBed, inject, async, fakeAsync, tick }
+	from '@angular/core/testing';
 import { BaseRequestOptions, Response, Http, ResponseOptions }
 	from '@angular/http';
 import { MockBackend } from '@angular/http/testing';
@@ -61,11 +62,42 @@ describe('ColorsService', () => {
 		});
 
 		service.getColors().then(colors => {
-			console.log(colors);
 			expect(colors).toEqual([['0-0', '0-1'], ['1-0', '1-1', '1-2', '1-3',
 			'1-4', '1-5', '1-6', '1-7', '1-8', '1-9'], ['2-0', '2-1', '2-2',
 			'2-3', '2-4', '2-5']]);
 			done();
 		});
 	})() });
+
+	it('should query the correct url', inject([ColorsService, MockBackend],
+		(service: ColorsService, backend: MockBackend) => {
+		let lastConnection;
+		backend.connections.subscribe(
+			(connection: any) => lastConnection = connection);
+		service.getColors();
+		expect(lastConnection).toBeDefined(
+			'no http service connection at all?');
+		expect(lastConnection.request.url).toMatch('api/colors/',
+			'url valid');
+	}));
+
+	it('should return an error when server is down', fakeAsync(() => {
+		inject([ColorsService, MockBackend],
+			(service: ColorsService, backend: MockBackend) => {
+			let result;
+			let catchedError;
+			backend.connections.subscribe(connection => {
+				connection.mockRespond(new Response(new ResponseOptions({
+					status: 404,
+					statusText: 'URL not found',
+				})));
+			});
+			service.getColors()
+				.then(colors => result = colors)
+				.catch(error => catchedError = error);
+			tick();
+			expect(result).toBeUndefined();
+			expect(catchedError).toBeDefined();
+		})()
+	}));
 });
