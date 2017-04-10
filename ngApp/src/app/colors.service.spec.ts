051b924b98133eb00c40818cfc281d50392890d9
XeryusTC <armadillo@onenetbeyond.org>
2017-04-10 14:35:06 +0200

    Code deduplication in Color Service [skip ci]

--- ngApp/src/app/colors.service.spec.ts
+++ ngApp/src/app/colors.service.spec.ts
@@ -7,7 +7,12 @@ import { MockBackend } from '@angular/http/testing';
 import { ColorsService } from './colors.service';
 
 describe('ColorsService', () => {
-	beforeEach(async() => {
+	let service: ColorsService;
+	let backend: MockBackend;
+	let conn;
+	let result;
+
+	beforeEach(() => {
 		TestBed.configureTestingModule({
 			providers: [
 				ColorsService,
@@ -21,83 +26,57 @@ describe('ColorsService', () => {
 				BaseRequestOptions
 			]
 		});
+
+		service = TestBed.get(ColorsService);
+		backend = TestBed.get(MockBackend);
+		backend.connections.subscribe(connection => conn = connection);
 	});
 
-	it('should ...', inject([ColorsService], (service: ColorsService) => {
-		expect(service).toBeTruthy();
+	it('should query the correct url', () => {
+		service.getColors();
+		expect(conn).toBeDefined('no http service connection at all?');
+		expect(conn.request.url).toMatch('api/colors/', 'url valid');
+	});
+
+	it('should return an error when server is down', fakeAsync(() => {
+		let catchedError;
+		service.getColors()
+			.then(colors => result = colors)
+			.catch(error => catchedError = error);
+		conn.mockRespond(new Response(new ResponseOptions({
+			status: 404,
+			statusText: 'URL not found',
+		})));
+		tick();
+		expect(catchedError).toBeDefined();
+		expect(result).toBeUndefined();
 	}));
 
-	it('should retrieve the list of colors', done => {
-		inject([ColorsService, MockBackend],
-			(service: ColorsService, backend: MockBackend) => {
+	it('should retrieve the list of colors', fakeAsync(() => {
 		let response = JSON.stringify([
 			['black', 'Black'],
 			['white', 'White'],
 			['red-500', 'Red 500']
 		]);
-		backend.connections.subscribe(connection => {
-			connection.mockRespond(new Response(new ResponseOptions(
-				{body: response})));
-		});
 
-		service.getColors().then(colors => {
-			expect(colors[0]).toContain('black');
-			expect(colors[0]).toContain('white',);
-			expect(colors[1]).toContain('red-500');
-			done();
-		});
-	})() });
-
-	it('should move the colors in a 10 wide grid', done => {
-		inject([ColorsService, MockBackend],
-			(service: ColorsService, backend: MockBackend) => {
-			let response = JSON.stringify([['0-0', ''], ['0-1', ''],
-				['1-0', ''], ['1-1', ''], ['1-2', ''], ['1-3', ''],
-				['1-4', ''], ['1-5', ''], ['1-6', ''], ['1-7', ''],
-				['1-8', ''], ['1-9', ''], ['2-0', ''], ['2-1', ''],
-				['2-2', ''], ['2-3', ''], ['2-4', ''], ['2-5', '']]);
-		backend.connections.subscribe(connection => {
-			connection.mockRespond(new Response(new ResponseOptions(
-				{body: response})));
-		});
-
-		service.getColors().then(colors => {
-			expect(colors).toEqual([['0-0', '0-1'], ['1-0', '1-1', '1-2', '1-3',
-			'1-4', '1-5', '1-6', '1-7', '1-8', '1-9'], ['2-0', '2-1', '2-2',
-			'2-3', '2-4', '2-5']]);
-			done();
-		});
-	})() });
-
-	it('should query the correct url', inject([ColorsService, MockBackend],
-		(service: ColorsService, backend: MockBackend) => {
-		let lastConnection;
-		backend.connections.subscribe(
-			(connection: any) => lastConnection = connection);
-		service.getColors();
-		expect(lastConnection).toBeDefined(
-			'no http service connection at all?');
-		expect(lastConnection.request.url).toMatch('api/colors/',
-			'url valid');
+		service.getColors().then(colors => result = colors);
+		conn.mockRespond(new Response(new ResponseOptions({body: response})));
+		tick();
+		expect(result[0]).toContain('black');
+		expect(result[0]).toContain('white',);
+		expect(result[1]).toContain('red-500');
 	}));
 
-	it('should return an error when server is down', fakeAsync(() => {
-		inject([ColorsService, MockBackend],
-			(service: ColorsService, backend: MockBackend) => {
-			let result;
-			let catchedError;
-			backend.connections.subscribe(connection => {
-				connection.mockRespond(new Response(new ResponseOptions({
-					status: 404,
-					statusText: 'URL not found',
-				})));
-			});
-			service.getColors()
-				.then(colors => result = colors)
-				.catch(error => catchedError = error);
-			tick();
-			expect(result).toBeUndefined();
-			expect(catchedError).toBeDefined();
-		})()
+	it('should move the colors in a 10 wide grid', fakeAsync(() => {
+		let response = JSON.stringify([['0-0', ''], ['0-1', ''], ['1-0', ''],
+			 ['1-1', ''], ['1-2', ''], ['1-3', ''], ['1-4', ''], ['1-5', ''],
+			 ['1-6', ''], ['1-7', ''], ['1-8', ''], ['1-9', ''], ['2-0', ''],
+			 ['2-1', ''], ['2-2', ''], ['2-3', ''], ['2-4', ''], ['2-5', '']]);
+		service.getColors().then(colors => result = colors);
+		conn.mockRespond(new Response(new ResponseOptions({body: response})));
+		tick();
+		expect(result).toEqual([['0-0', '0-1'], ['1-0', '1-1', '1-2', '1-3',
+			'1-4', '1-5', '1-6', '1-7', '1-8', '1-9'], ['2-0', '2-1', '2-2',
+			'2-3', '2-4', '2-5']]);
 	}));
 });
