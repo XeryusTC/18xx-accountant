35e65de3510757ce043940d9176a0c4dc8e624fe
XeryusTC <armadillo@onenetbeyond.org>
2017-04-19 01:06:04 +0200

    Menu uses the game state service

--- ngApp/src/app/menu/menu.component.spec.ts
+++ ngApp/src/app/menu/menu.component.spec.ts
@@ -1,17 +1,27 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule }              from '@angular/router/testing';
 
-import { MenuComponent } from './menu.component';
 import { RouterLinkStubDirective } from '../testing/router-stubs';
+
+import { Game }             from '../models/game';
+import { MenuComponent }    from './menu.component';
+import { GameStateService } from '../game-state.service';
 
 describe('MenuComponent', () => {
 	let component: MenuComponent;
 	let fixture: ComponentFixture<MenuComponent>;
+	let gameServiceStub = {
+		loadGame: (() => {}),
+		game: new Game('test-game-uuid', 12000)
+	};
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
 			imports: [RouterTestingModule],
-			declarations: [ MenuComponent, RouterLinkStubDirective ]
+			declarations: [ MenuComponent, RouterLinkStubDirective ],
+			providers: [
+				{provide: GameStateService, useValue: gameServiceStub}
+			]
 		})
 		.compileComponents();
 	}));
--- ngApp/src/app/menu/menu.component.ts
+++ ngApp/src/app/menu/menu.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { GameStateService } from '../game-state.service';
+
 @Component({
 	selector: 'menu',
 	templateUrl: './menu.component.html',
 	styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-	@Input() game_id: string;
-
-	constructor() { }
+	constructor(private gameState: GameStateService) { }
 
 	ngOnInit() {
 	}
