31284a5678cb90a2f9dca85b24c25323afe711aa
XeryusTC <armadillo@onenetbeyond.org>
2017-04-22 13:00:56 +0200

    Deploy code and install dependencies

--- build-ng-app.sh
+++ build-ng-app.sh
@@ -4,5 +4,5 @@ ng build $@
 cd ..
 mkdir -p accountant/templates/ng/
 mkdir -p accountant/static/ng/
-mv -v ngApp/dist/index.html accountant/templates/ng/index.html
+cp -v ngApp/dist/index.html accountant/templates/ng/index.html
 cp -v ngApp/dist/*.js accountant/static/ng/
