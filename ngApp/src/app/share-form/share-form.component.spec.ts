8201aff4b1df8adeb54c2251d6ae9173b9481f3a
XeryusTC <armadillo@onenetbeyond.org>
2017-04-24 23:22:19 +0200

    Test what can be transfered to where

--- ngApp/src/app/share-form/share-form.component.spec.ts
+++ ngApp/src/app/share-form/share-form.component.spec.ts
@@ -1,14 +1,52 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By }                               from '@angular/platform-browser';
+import { FormsModule }                      from '@angular/forms';
 
-import { ShareFormComponent } from './share-form.component';
+import { Company }              from '../models/company';
+import { Player }               from '../models/player';
+import { GameService }          from '../game.service';
+import { GameStateService }     from '../game-state.service';
+import { ShareFormComponent }   from './share-form.component';
+import { TransferShareService } from '../transfer-share.service';
+import { ValuesPipe }           from '../values.pipe';
 
 describe('ShareFormComponent', () => {
 	let component: ShareFormComponent;
 	let fixture: ComponentFixture<ShareFormComponent>;
+	let gameStateStub;
+	let transferShareStub;
+
+	let buyCompany = new Company('buy-uuid', 'game-uuid', 'RDR', 100, 10),
+		shareCompany = new Company('share-uuid', 'game-uuid', 'PMQ', 0, 10),
+		sourceCompany = new Company('source-uuid', 'game-uuid', 'NNH', 50, 10);
+	let buyPlayer = new Player('buy-player', 'game-uuid', 'Alice', 200),
+		sourcePlayer = new Player('source-player', 'game-uuid', 'Charlie', 10);
+
+	function submitForm() {
+		fixture.debugElement.query(By.css('form'))
+			.triggerEventHandler('submit', new Event('submit'));
+	}
 
 	beforeEach(async(() => {
+		gameStateStub = jasmine
+			.createSpyObj('GameStateService', ['updateGame', 'updatePlayer',
+						  'updateCompany', 'companies']);
+		gameStateStub.companies = {
+			[buyCompany.uuid]: buyCompany,
+			[shareCompany.uuid]: shareCompany,
+			[sourceCompany.uuid]: sourceCompany
+		};
+
+		transferShareStub = jasmine
+			.createSpyObj('TransferShareService', ['transferShare']);
+
 		TestBed.configureTestingModule({
-			declarations: [ ShareFormComponent ]
+			imports: [ FormsModule ],
+			declarations: [ ShareFormComponent, ValuesPipe ],
+			providers: [
+				{provide: GameStateService, useValue: gameStateStub},
+				{provide: TransferShareService, useValue: transferShareStub}
+			]
 		})
 		.compileComponents();
 	}));
@@ -16,10 +54,11 @@ describe('ShareFormComponent', () => {
 	beforeEach(() => {
 		fixture = TestBed.createComponent(ShareFormComponent);
 		component = fixture.componentInstance;
-		fixture.detectChanges();
+		component.company_share = shareCompany.uuid;
 	});
 
 	it('should create', () => {
+		fixture.detectChanges();
 		expect(component).toBeTruthy();
 	});
 
@@ -27,7 +66,84 @@ describe('ShareFormComponent', () => {
 		expect(component.source).toBe('ipo');
 	});
 
-	it('amount is one by default', () => {
-		expect(component.shares).toBe(1);
+	it('amount of shares is one by default', () => {
+		expect(component.share_amount).toBe(1);
+	});
+
+	it('onSubmit() uses correct amount of shares', () => {
+		component.share_amount = 42;
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[4]).toBe(42);
+	});
+
+	it('onSubmit() allows the ipo to buy a share', () => {
+		component.buyer = 'ipo';
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[0]).toBe('ipo');
+	});
+
+	it('onSubmit() allows the bank pool to buy a share', () => {
+		component.buyer = 'bank';
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[0]).toBe('bank');
+	});
+
+	it('onSubmit() allows a company to buy a share', () => {
+		component.buyer = buyCompany;
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[0]).toBe(buyCompany);
+	});
+
+	it('onSubmit() allows a player to buy a share', () => {
+		component.buyer = buyPlayer;
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[0]).toBe(buyPlayer);
+	});
+
+	it('onSubmit() allows a share to be bought from the ipo', () => {
+		component.source = 'ipo';
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[2]).toBe('ipo');
+	});
+
+	it('onSubmit() allows a share to be bought from the bank pool', () => {
+		component.source = 'bank';
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[2]).toBe('bank');
+	});
+
+	it('onSubmit() allows a share to be bought from a company', () => {
+		component.source = sourceCompany;
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[2]).toBe(sourceCompany);
+	});
+
+	it('onSubmit() allows a share to be bought from a player', () => {
+		component.source = sourcePlayer;
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[2]).toBe(sourcePlayer);
+	});
+
+	it('onSubmit() correctly sets the company a share is bought in', () => {
+		component.company_share = shareCompany.uuid;
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[1]).toBe(shareCompany);
+	});
+
+	it('onSubmit() currectly sets the price of the share', () => {
+		shareCompany.value = 82;
+		submitForm();
+		let args = transferShareStub.transferShare.calls.first().args;
+		expect(args[3]).toBe(82);
 	});
 });
--- ngApp/src/app/share-form/share-form.component.ts
+++ ngApp/src/app/share-form/share-form.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 
-import { GameStateService } from '../game-state.service';
+import { Company }              from '../models/company';
+import { Player }               from '../models/player';
+import { GameStateService }     from '../game-state.service';
+import { TransferShareService } from '../transfer-share.service';
 
 @Component({
 	selector: 'share-form',
@@ -8,12 +11,24 @@ import { GameStateService } from '../game-state.service';
 	styleUrls: ['./share-form.component.css']
 })
 export class ShareFormComponent implements OnInit {
-	shares: number = 1;
-	source: string = 'ipo';
+	company_share: string;
+	share_amount: number = 1;
+	source: Company | Player | string = 'ipo';
 	@Input() buyer;
 
-	constructor(private gameState: GameStateService) { }
+	constructor(
+		private gameState: GameStateService,
+		private transferShareService: TransferShareService
+	) { }
 
 	ngOnInit() {
+	}
+
+	onSubmit(e: Event) {
+		e.preventDefault();
+		let company_share = this.gameState.companies[this.company_share];
+		this.transferShareService
+			.transferShare(this.buyer, company_share, this.source,
+						   company_share.value, this.share_amount);
 	}
 }
