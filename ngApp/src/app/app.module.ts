83b4b0b705acabc09f179e3a470040b249399a7f
XeryusTC <armadillo@onenetbeyond.org>
2017-05-14 11:37:40 +0200

    Create order in the app module

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -3,56 +3,58 @@ import { NgModule }             from '@angular/core';
 import { FormsModule }          from '@angular/forms';
 import { HttpModule }           from '@angular/http';
 
+import { CompanyService }       from './company.service';
 import { GameService }          from './game.service';
 import { GameStateService }     from './game-state.service';
+import { OperateService }       from './operate.service';
 import { PlayerService }        from './player.service';
-import { CompanyService }       from './company.service';
+import { ShareService }         from './share.service';
 import { TransferMoneyService } from './transfer-money.service';
 import { TransferShareService } from './transfer-share.service';
-import { ShareService }         from './share.service';
-import { OperateService }       from './operate.service';
 
 import { ValuesPipe } from './values.pipe';
 
-import { AddCompanyComponent }     from './add-company/add-company.component';
-import { AddCompanyFormComponent } from
-	'./add-company-form/add-company-form.component';
-import { AddPlayerComponent }      from './add-player/add-player.component';
-import { AddPlayerFormComponent }  from
-	'./add-player-form/add-player-form.component';
-import { AppComponent }            from './app.component';
-import { BankDetailComponent }     from './bank-detail/bank-detail.component';
-import { GameRoutingModule }       from './game-routing.module';
-import { GamePageComponent }       from './game-page/game-page.component';
-import { MenuComponent }           from './menu/menu.component';
-import { StartPageComponent }      from './start-page/start-page.component';
-import { StartGameFormComponent }  from
-	'./start-game-form/start-game-form.component';
-import { TransferFormComponent }   from
-	'./transfer-form/transfer-form.component';
-import { ShareFormComponent } from './share-form/share-form.component';
-import { PlayerSectionComponent } from './player-section/player-section.component';
-import { CompanySectionComponent } from './company-section/company-section.component';
+import { AddCompanyComponent }  from './add-company/add-company.component';
+import { AddCompanyFormComponent }
+	from './add-company-form/add-company-form.component';
+import { AddPlayerComponent }   from './add-player/add-player.component';
+import { AddPlayerFormComponent }
+	from './add-player-form/add-player-form.component';
+import { AppComponent }         from './app.component';
+import { BankDetailComponent }  from './bank-detail/bank-detail.component';
+import { CompanySectionComponent }
+	from './company-section/company-section.component';
+import { GamePageComponent }    from './game-page/game-page.component';
+import { GameRoutingModule }    from './game-routing.module';
+import { MenuComponent }        from './menu/menu.component';
 import { OperateFormComponent } from './operate-form/operate-form.component';
+import { PlayerSectionComponent }
+	from './player-section/player-section.component';
+import { ShareFormComponent }   from './share-form/share-form.component';
+import { StartGameFormComponent }
+	from './start-game-form/start-game-form.component';
+import { StartPageComponent }   from './start-page/start-page.component';
+import { TransferFormComponent }
+	from './transfer-form/transfer-form.component';
 
 @NgModule({
 	declarations: [
-		AppComponent,
-		StartGameFormComponent,
-		StartPageComponent,
-		GamePageComponent,
-		BankDetailComponent,
-		MenuComponent,
-		AddPlayerComponent,
-		AddPlayerFormComponent,
 		AddCompanyComponent,
 		AddCompanyFormComponent,
-		ValuesPipe,
-		TransferFormComponent,
-		ShareFormComponent,
-		PlayerSectionComponent,
+		AddPlayerComponent,
+		AddPlayerFormComponent,
+		AppComponent,
+		BankDetailComponent,
 		CompanySectionComponent,
-		OperateFormComponent
+		GamePageComponent,
+		MenuComponent,
+		OperateFormComponent,
+		PlayerSectionComponent,
+		ShareFormComponent,
+		StartGameFormComponent,
+		StartPageComponent,
+		TransferFormComponent,
+		ValuesPipe,
 	],
 	imports: [
 		BrowserModule,
@@ -61,15 +63,15 @@ import { OperateFormComponent } from './operate-form/operate-form.component';
 		GameRoutingModule
 	],
 	providers: [
+		CompanyService,
 		GameStateService,
 		GameService,
-		PlayerService,
-		CompanyService,
-		TransferMoneyService,
-		TransferShareService,
-		ShareService,
 		OperateService,
-		Title
+		PlayerService,
+		ShareService,
+		Title,
+		TransferMoneyService,
+		TransferShareService
 	],
 	bootstrap: [AppComponent]
 })
