66a0defb8f08fa858fe09a9424ac0c93bb3b8df4
XeryusTC <armadillo@onenetbeyond.org>
2017-07-30 03:31:50 +0200

    Show company colors in log section

--- ngApp/src/app/log-section/log-section.component.spec.ts
+++ ngApp/src/app/log-section/log-section.component.spec.ts
@@ -1,12 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { LogEntry }            from '../models/log-entry';
 import { GameStateService }    from '../game-state.service';
 import { LogSectionComponent } from './log-section.component';
 
 describe('LogSectionComponent', () => {
 	let component: LogSectionComponent;
 	let fixture: ComponentFixture<LogSectionComponent>;
-	let gameStateStub = {};
+	let gameStateStub = {
+		companies: {
+			'company-uuid': {text_color: 'red-50', background_color: 'red-900'}
+		}
+	};
 
 	beforeEach(async(() => {
 		TestBed.configureTestingModule({
@@ -26,5 +31,19 @@ describe('LogSectionComponent', () => {
 	it('should create', () => {
 		fixture.detectChanges();
 		expect(component).toBeTruthy();
+	});
+
+	it('entryColorClass should use company colors', () => {
+		let entry = new LogEntry('entry-uuid', 'game-uuid',
+								 new Date(2017, 7, 30), 'test entry',
+								 'company-uuid');
+		expect(component.entryColorClass(entry)).toBe('fg-red-50 bg-red-900');
+	});
+
+	it('entryColorClass should not include colors when no acting company',
+	   () => {
+		let entry = new LogEntry('entry-uuid', 'game-uuid',
+								 new Date(2017, 7, 30), 'test entry', null);
+		expect(component.entryColorClass(entry)).toBe('');
 	});
 });
--- ngApp/src/app/log-section/log-section.component.ts
+++ ngApp/src/app/log-section/log-section.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import { LogEntry }         from '../models/log-entry';
 import { GameStateService } from '../game-state.service';
 
 @Component({
@@ -11,5 +12,15 @@ export class LogSectionComponent implements OnInit {
 	constructor(public gameState: GameStateService) { }
 
 	ngOnInit() {
+	}
+
+	entryColorClass(entry: LogEntry): string {
+		if (entry.acting_company === null) {
+			return '';
+		} else {
+			let company = this.gameState.companies[entry.acting_company];
+			return 'fg-' + company.text_color +
+				' bg-' + company.background_color;
+		}
 	}
 }
