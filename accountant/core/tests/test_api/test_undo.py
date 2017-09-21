568fb338484f1632b8119298cbc3cf9b5b9fb2c6
XeryusTC <armadillo@onenetbeyond.org>
2017-09-21 20:19:06 +0200

    Unit test for proper redo reply on transfer money

--- accountant/core/tests/test_api/test_undo.py
+++ accountant/core/tests/test_api/test_undo.py
@@ -153,6 +153,8 @@ class RedoTests(APITestCase):
             text='New game started')
         self.game.log_cursor = self.start_entry
         self.game.save()
+        self.player = factories.PlayerFactory(game=self.game, cash=100)
+        self.company = factories.CompanyFactory(game=self.game, cash=100)
         self.url = reverse('undo')
 
     def test_GET_request_is_empty(self):
@@ -162,41 +164,112 @@ class RedoTests(APITestCase):
         self.assertIsNone(response.data)
 
     def test_redoing_player_to_bank_money_transfer_includes_data(self):
-        player = factories.PlayerFactory(game=self.game, cash=100)
         entry = models.LogEntry.objects.create(game=self.game,
-            acting_player=player, amount=1,
+            acting_player=self.player, amount=1,
             action=models.LogEntry.TRANSFER_MONEY)
 
         response = self.client.post(self.url, {'game': str(self.game.pk),
             'action': 'redo'})
 
         self.game.refresh_from_db()
-        player.refresh_from_db()
+        self.player.refresh_from_db()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['game']['uuid'], str(self.game.pk))
         self.assertEqual(response.data['game']['cash'], 1001)
         self.assertNotIn('companies', response.data.keys())
         self.assertEqual(len(response.data['players']), 1)
-        self.assertEqual(response.data['players'][0]['uuid'], str(player.pk))
+        self.assertEqual(response.data['players'][0]['uuid'],
+            str(self.player.pk))
         self.assertEqual(response.data['players'][0]['cash'], 99)
         self.assertEqual(response.data['log']['uuid'], str(entry.pk))
 
     def test_redoing_company_to_bank_money_transfer_includes_data(self):
-        company = factories.CompanyFactory(game=self.game, cash=100)
         entry = models.LogEntry.objects.create(game=self.game,
-            action=models.LogEntry.TRANSFER_MONEY, acting_company=company,
+            action=models.LogEntry.TRANSFER_MONEY, acting_company=self.company,
             amount=2)
         response = self.client.post(self.url, {'game': str(self.game.pk),
             'action': 'redo'})
 
         self.game.refresh_from_db()
-        company.refresh_from_db()
+        self.company.refresh_from_db()
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['game']['uuid'], str(self.game.pk))
         self.assertEqual(response.data['game']['cash'], 1002)
         self.assertNotIn('players', response.data.keys())
         self.assertEqual(len(response.data['companies']), 1)
         self.assertEqual(response.data['companies'][0]['uuid'],
-            str(company.pk))
+            str(self.company.pk))
         self.assertEqual(response.data['companies'][0]['cash'], 98)
+        self.assertEqual(response.data['log']['uuid'], str(entry.pk))
+
+    def test_redoing_player_to_player_money_transfer_includes_data(self):
+        other_player = factories.PlayerFactory(game=self.game)
+        entry = models.LogEntry.objects.create(game=self.game,
+            acting_player=self.player, receiving_player=other_player,
+            amount=3, action=models.LogEntry.TRANSFER_MONEY)
+        response = self.client.post(self.url, {'game': str(self.game.pk),
+            'action': 'redo'})
+
+        self.player.refresh_from_db()
+        other_player.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotIn('game', response.data.keys())
+        self.assertNotIn('companies', response.data.keys())
+        self.assertCountEqual([p['uuid'] for p in response.data['players']],
+            [str(self.player.pk), str(other_player.pk)])
+        self.assertEqual(response.data['log']['uuid'], str(entry.pk))
+
+    def test_redoing_player_to_company_money_transfer_includes_data(self):
+        entry = models.LogEntry.objects.create(game=self.game,
+            acting_player=self.player, receiving_company=self.company,
+            amount=4, action=models.LogEntry.TRANSFER_MONEY)
+        response = self.client.post(self.url, {'game': str(self.game.pk),
+            'action': 'redo'})
+
+        self.player.refresh_from_db()
+        self.company.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotIn('game', response.data.keys())
+        self.assertEqual(len(response.data['players']), 1)
+        self.assertEqual(len(response.data['companies']), 1)
+        self.assertEqual(response.data['players'][0]['uuid'],
+            str(self.player.pk))
+        self.assertEqual(response.data['companies'][0]['uuid'],
+            str(self.company.pk))
+        self.assertEqual(response.data['log']['uuid'], str(entry.pk))
+
+    def test_redoing_company_to_player_money_transfer_includes_data(self):
+        entry = models.LogEntry.objects.create(game=self.game,
+            acting_company=self.company, receiving_player=self.player,
+            amount=5, action=models.LogEntry.TRANSFER_MONEY)
+        response = self.client.post(self.url, {'game': str(self.game.pk),
+            'action': 'redo'})
+
+        self.player.refresh_from_db()
+        self.company.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotIn('game', response.data.keys())
+        self.assertEqual(len(response.data['players']), 1)
+        self.assertEqual(len(response.data['companies']), 1)
+        self.assertEqual(response.data['players'][0]['uuid'],
+            str(self.player.pk))
+        self.assertEqual(response.data['companies'][0]['uuid'],
+            str(self.company.pk))
+        self.assertEqual(response.data['log']['uuid'], str(entry.pk))
+
+    def test_redoing_company_to_company_money_transfer_includes_data(self):
+        other_company = factories.CompanyFactory(game=self.game)
+        entry = models.LogEntry.objects.create(game=self.game,
+            acting_company=self.company, receiving_company=other_company,
+            amount=6, action=models.LogEntry.TRANSFER_MONEY)
+        response = self.client.post(self.url, {'game': str(self.game.pk),
+            'action': 'redo'})
+
+        self.company.refresh_from_db()
+        other_company.refresh_from_db()
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertNotIn('game', response.data.keys())
+        self.assertNotIn('players', response.data.keys())
+        self.assertCountEqual([c['uuid'] for c in response.data['companies']],
+            [str(self.company.pk), str(other_company.pk)])
         self.assertEqual(response.data['log']['uuid'], str(entry.pk))
--- accountant/core/tests/test_undo.py
+++ accountant/core/tests/test_undo.py
@@ -229,24 +229,79 @@ class RedoTransferMoneyTests(TestCase):
             mock_transfer_money):
         entry = models.LogEntry.objects.create(game=self.game,
             action=models.LogEntry.TRANSFER_MONEY, acting_company=self.company,
-            amount=10)
+            amount=20)
 
         utils.redo(self.game)
 
         self.game.refresh_from_db()
-        mock_transfer_money.assert_called_once_with(self.company, None, 10)
+        mock_transfer_money.assert_called_once_with(self.company, None, 20)
+        self.assertEqual(self.game.log_cursor, entry)
+
+    def test_can_redo_player_transfering_money_to_player(self,
+            mock_transfer_money):
+        other_player = factories.PlayerFactory(game=self.game)
+        entry = models.LogEntry.objects.create(game=self.game,
+            action=models.LogEntry.TRANSFER_MONEY, acting_player=self.player,
+            receiving_player=other_player, amount=30)
+
+        utils.redo(self.game)
+
+        self.game.refresh_from_db()
+        mock_transfer_money.assert_called_once_with(self.player, other_player,
+            30)
+        self.assertEqual(self.game.log_cursor, entry)
+
+    def test_can_redo_player_transfering_money_to_company(self,
+            mock_transfer_money):
+        entry = models.LogEntry.objects.create(game=self.game,
+            action=models.LogEntry.TRANSFER_MONEY, acting_player=self.player,
+            receiving_company=self.company, amount=40)
+
+        utils.redo(self.game)
+
+        self.game.refresh_from_db()
+        mock_transfer_money.assert_called_once_with(self.player, self.company,
+            40)
+        self.assertEqual(self.game.log_cursor, entry)
+
+    def test_can_redo_company_transfering_money_to_player(self,
+            mock_transfer_money):
+        entry = models.LogEntry.objects.create(game=self.game,
+            action=models.LogEntry.TRANSFER_MONEY, acting_company=self.company,
+            receiving_player=self.player, amount=50)
+
+        utils.redo(self.game)
+
+        self.game.refresh_from_db()
+        mock_transfer_money.assert_called_once_with(self.company, self.player,
+            50)
+        self.assertEqual(self.game.log_cursor, entry)
+
+    def test_can_redo_company_transfering_money_to_company(self,
+            mock_transfer_money):
+        other_company = factories.CompanyFactory(game=self.game)
+        entry = models.LogEntry.objects.create(game=self.game,
+            action=models.LogEntry.TRANSFER_MONEY, acting_company=self.company,
+            receiving_company=other_company, amount=60)
+
+        utils.redo(self.game)
+
+        self.game.refresh_from_db()
+        mock_transfer_money.assert_called_once_with(self.company,
+            other_company, 60)
         self.assertEqual(self.game.log_cursor, entry)
 
     def test_redo_player_transfer_money_to_bank_returns_affected_instances(
             self, mock_transfer_money):
         entry = models.LogEntry.objects.create(game=self.game,
             action=models.LogEntry.TRANSFER_MONEY, acting_player=self.player,
-            amount=20)
+            amount=70)
 
         affected = utils.redo(self.game)
 
         self.game.refresh_from_db()
         self.player.refresh_from_db()
+        self.assertNotIn('companies', affected.keys())
         self.assertEqual(affected['game'], self.game)
         self.assertEqual(affected['players'], [self.player])
         self.assertEqual(affected['log'], self.game.log_cursor)
@@ -255,12 +310,76 @@ class RedoTransferMoneyTests(TestCase):
             self, mock_transfer_money):
         entry = models.LogEntry.objects.create(game=self.game,
             action=models.LogEntry.TRANSFER_MONEY, acting_company=self.company,
-            amount=20)
+            amount=80)
 
         affected = utils.redo(self.game)
 
         self.game.refresh_from_db()
         self.company.refresh_from_db()
+        self.assertNotIn('players', affected.keys())
         self.assertEqual(affected['game'], self.game)
         self.assertEqual(affected['companies'], [self.company])
         self.assertEqual(affected['log'], self.game.log_cursor)
+
+    def test_redo_player_transfer_money_to_player_returns_affected_instances(
+            self, mock_transfer_money):
+        other_player = factories.PlayerFactory(game=self.game)
+        entry = models.LogEntry.objects.create(game=self.game,
+            action=models.LogEntry.TRANSFER_MONEY, amount=90,
+            acting_player=self.player, receiving_player=other_player)
+
+        affected = utils.redo(self.game)
+
+        self.player.refresh_from_db()
+        other_player.refresh_from_db()
+        self.assertNotIn('game', affected.keys())
+        self.assertNotIn('companies', affected.keys())
+        self.assertEqual(affected['log'], self.game.log_cursor)
+        self.assertCountEqual(affected['players'], [self.player, other_player])
+
+    def test_redo_player_transfer_money_to_company_returns_affected_instances(
+            self, mock_transfer_money):
+        entry = models.LogEntry.objects.create(game=self.game,
+            action=models.LogEntry.TRANSFER_MONEY, amount=90,
+            acting_player=self.player, receiving_company=self.company)
+
+        affected = utils.redo(self.game)
+
+        self.player.refresh_from_db()
+        self.company.refresh_from_db()
+        self.assertNotIn('game', affected.keys())
+        self.assertEqual(affected['log'], self.game.log_cursor)
+        self.assertEqual(affected['players'], [self.player])
+        self.assertEqual(affected['companies'], [self.company])
+
+    def test_redo_company_transfer_money_to_player_returns_affected_instances(
+            self, mock_transfer_money):
+        entry = models.LogEntry.objects.create(game=self.game,
+            action=models.LogEntry.TRANSFER_MONEY, amount=90,
+            acting_company=self.company, receiving_player=self.player)
+
+        affected = utils.redo(self.game)
+
+        self.player.refresh_from_db()
+        self.company.refresh_from_db()
+        self.assertNotIn('game', affected.keys())
+        self.assertEqual(affected['log'], self.game.log_cursor)
+        self.assertEqual(affected['players'], [self.player])
+        self.assertEqual(affected['companies'], [self.company])
+
+    def test_redo_company_transfer_money_to_company_returns_affected_instances(
+            self, mock_transfer_money):
+        other_company = factories.CompanyFactory(game=self.game)
+        entry = models.LogEntry.objects.create(game=self.game,
+            action=models.LogEntry.TRANSFER_MONEY, amount=90,
+            acting_company=self.company, receiving_company=other_company)
+
+        affected = utils.redo(self.game)
+
+        self.company.refresh_from_db()
+        other_company.refresh_from_db()
+        self.assertNotIn('game', affected.keys())
+        self.assertNotIn('players', affected.keys())
+        self.assertEqual(affected['log'], self.game.log_cursor)
+        self.assertCountEqual(affected['companies'],
+            [self.company, other_company])
