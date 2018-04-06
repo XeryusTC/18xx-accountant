f769ef4d00dac18d6a1ddfbd6a8ad3d652fd6e5d
XeryusTC <armadillo@onenetbeyond.org>
2018-04-06 17:36:47 +0200

    Show angular version if it is invalid

--- accountant/functional_tests/test_homepage.py
+++ accountant/functional_tests/test_homepage.py
@@ -13,7 +13,9 @@ class HomePageTest(FunctionalTestCase):
         page = game.Homepage(self.browser)
         self.assertIsNotNone(page.app_root)
         self.assertTrue(
-            page.app_root.get_attribute('ng-version').startswith('5.1'))
+            page.app_root.get_attribute('ng-version').startswith('5.1'),
+            'Invalid Angular {} expected 5.1.x'.format(
+                page.app_root.get_attribute('ng-version')))
 
     def test_create_game(self):
         self.story('Alice is a user who visits the website')
